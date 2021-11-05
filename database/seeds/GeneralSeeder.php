fa719f61bf889f5786bc0d43c86dd1796b3e90b7
Sonu <bilawal.jabbar.786@gmail.com>
2021-11-06 00:19:08 +0500

    first commit

---
+++ database/seeds/GeneralSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class GeneralSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'fname' => 'Admin',
+            'lname' => '',
+            'email' => 'admin@gmail.com',
+            'role' => '0',
+            'phone' => '00000000',
+            'address' => 'Guadeloupe',
+            'password' => Hash::make('12345678'),
+        ]);
+    }
+}
